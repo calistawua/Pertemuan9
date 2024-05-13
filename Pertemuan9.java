cb5a885baec877696d9a8b21b9e0e159ec25b550
calistawua <127809356+calistawua@users.noreply.github.com>
2024-05-13 12:15:58 +0800

    Add files via upload

---
+++ Pertemuan9.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pertemuan9;
+
+/**
+ *
+ * @author calista2201010294
+ * Tgl: 2024-05-13
+ */
+public class Pertemuan9 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new frm01().setVisible(true);
+        frm01 f1 = new frm01();
+        f1.setVisible(true);
+        f1.setAlwaysOnTop(true);
+    }
+    
+}
